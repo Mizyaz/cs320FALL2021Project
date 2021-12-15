b51cc55d72f9606b3cc45ba10989b924af1a6ea1
guven <islam.guven@ozu.edu.tr>
2021-12-15 19:58:04 +0300

    added click listeners to recycler view objects

--- app/src/main/java/com/example/android/navigation/MyItemRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/android/navigation/MyItemRecyclerViewAdapter.kt
@@ -5,31 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 
 import com.example.android.navigation.placeholder.PlaceholderContent.PlaceholderItem
 import com.example.android.navigation.databinding.FragmentMainTasksPageBinding
 
-/**
- * [RecyclerView.Adapter] that can display a [PlaceholderItem].
- * TODO: Replace the implementation with code for your data type.
- */
 class MyItemRecyclerViewAdapter(
     private val values: List<PlaceholderItem>
-) : RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<MyItemRecyclerViewAdapter.ItemViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder{
+        // create a new view
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_main_tasks_page, parent, false)
 
-        return ViewHolder(
-            FragmentMainTasksPageBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
+        val holder = ItemViewHolder(adapterLayout)
 
+
+        holder.itemView.setOnClickListener{ view ->
+            view.findNavController().navigate(R.id.action_mainTasksPageFragment_to_individualTaskPage)
+        }
+
+        return holder
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = values[position]
         holder.idView.text = item.id
         holder.contentView.text = item.content
@@ -37,13 +39,12 @@ class MyItemRecyclerViewAdapter(
 
     override fun getItemCount(): Int = values.size
 
-    inner class ViewHolder(binding: FragmentMainTasksPageBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        val idView: TextView = binding.itemNumber
-        val contentView: TextView = binding.content
+    class ItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val idView: TextView = view.findViewById(R.id.item_number)
+        val contentView: TextView = view.findViewById(R.id.content)
 
-        override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
+        init {
+            // Define click listener for the ViewHolder's View.
         }
     }
 
