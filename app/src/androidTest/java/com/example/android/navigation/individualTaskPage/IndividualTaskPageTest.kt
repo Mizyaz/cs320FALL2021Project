1348adfb46f77017c735dbafc16546001cbd4297
Betül Seyhan <betul.seyhan@ozu.edu.tr>
2021-12-24 22:44:11 +0300

    .IndividualTaskPageTest is added

---
+++ app/src/androidTest/java/com/example/android/navigation/individualTaskPage/IndividualTaskPageTest.kt
@@ -0,0 +1,23 @@
+package com.example.android.navigation.individualTaskPage
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class IndividualTaskPageTest {
+
+    @org.junit.jupiter.api.Test
+    fun getBinding() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setBinding() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun onCreateView() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun onDestroyView() {
+    }
+}
